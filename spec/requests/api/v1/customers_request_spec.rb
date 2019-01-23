dc5d1fd47b2214840ff5a9e8690f6416736ebb5e
Leah Miller <lkmiller.87@gmail.com>
2019-01-23 12:31:16 -0700

    Adds updated passing tests for customer serializer

--- spec/requests/api/v1/customers_request_spec.rb
+++ spec/requests/api/v1/customers_request_spec.rb
@@ -9,15 +9,17 @@ describe "Customers API" do
 
     customers = JSON.parse(response.body)
 
-    expect(customers.count).to eq(3)
+    expect(customers.count).to eq(1)
+    expect(customers["data"].count).to eq(3)
+
   end
   it "can get one customer by its id" do
-    id = create(:customer).id
+    id = create(:customer).id.to_s
 
     get "/api/v1/customers/#{id}"
 
     customer = JSON.parse(response.body)
     expect(response).to be_successful
-    expect(customer["id"]).to eq(id)
+    expect(customer["data"]["id"]).to eq(id)
   end
 end

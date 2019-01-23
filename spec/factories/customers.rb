d7cccda02d11947884d8424c7852de635843f862
Leah Miller <lkmiller.87@gmail.com>
2019-01-23 10:52:44 -0700

    Adds passing tests for customers api index request

--- spec/factories/customers.rb
+++ spec/factories/customers.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :customer do
-    first_name { "MyString" }
-    last_name { "MyString" }
+    first_name { "Gob" }
+    last_name { "Bluth" }
   end
 end
---
+++ spec/requests/api/v1/customers_request_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+describe "Customers API" do
+  it "sends a lits of customers" do
+    create_list(:customer, 3)
+    get '/api/v1/customers'
+
+    expect(response).to be_successful
+  end
+end

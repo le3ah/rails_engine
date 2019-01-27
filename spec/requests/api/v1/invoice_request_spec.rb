78f10f00f36086f8d6a25d36dc26b1906e14cf44
Leah Miller <lkmiller.87@gmail.com>
2019-01-27 15:10:24 -0700

    Adds passing test for find invoice by created_at

--- spec/requests/api/v1/invoice_request_spec.rb
+++ spec/requests/api/v1/invoice_request_spec.rb
@@ -61,5 +61,15 @@ describe "Invoices API" do
       expect(response).to be_successful
       expect(invoice["data"]["id"]).to eq(invoice_1.id.to_s)
     end
+    it "can find a single object by created_at" do
+      merchant = create(:merchant)
+      customer = create(:customer)
+      invoice_1 = create(:invoice, merchant: merchant, customer: customer, created_at: "2012-03-25 09:54:09 UTC")
+      get "/api/v1/invoices/find?created_at=#{invoice_1.created_at}"
+
+      invoice = JSON.parse(response.body)
+      expect(response).to be_successful
+      expect(invoice["data"]["id"]).to eq(invoice_1.id.to_s)
+    end
   end
 end

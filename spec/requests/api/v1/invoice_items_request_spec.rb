303a7bcb98745f9ed559a4fed087a18998e4c213
Leah Miller <lkmiller.87@gmail.com>
2019-01-27 16:40:11 -0700

    Adds passing test for find all invoice items by unit_price

--- spec/requests/api/v1/invoice_items_request_spec.rb
+++ spec/requests/api/v1/invoice_items_request_spec.rb
@@ -182,5 +182,19 @@ describe "Invoice Items API" do
 
       expect(invoice_item["data"][0]["attributes"]["id"]).to eq(invoice_item_1.id)
     end
+    it "can find all matches based on unit_price" do
+      merchant_1 = create(:merchant)
+      customer_1 = create(:customer)
+      invoice_1 = create(:invoice, merchant: merchant_1, customer: customer_1)
+      item_1 = create(:item, merchant: merchant_1)
+      invoice_item_1 = create(:invoice_item, quantity: 1, unit_price: 50, item_id: item_1.id, invoice_id: invoice_1.id)
+
+      get "/api/v1/invoice_items/find_all?unit_price=#{invoice_item_1.unit_price}"
+
+      invoice_item = JSON.parse(response.body)
+      expect(response).to be_successful
+
+      expect(invoice_item["data"][0]["attributes"]["id"]).to eq(invoice_item_1.id)
+    end
   end
 end

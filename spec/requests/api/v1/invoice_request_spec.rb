9f454443c2653053c72ee66ae067be7675a6bdad
Leah Miller <lkmiller.87@gmail.com>
2019-01-27 16:30:34 -0700

    Adds passing test for find all invoices by merchant id

--- spec/requests/api/v1/invoice_request_spec.rb
+++ spec/requests/api/v1/invoice_request_spec.rb
@@ -111,5 +111,19 @@ describe "Invoices API" do
 
       expect(invoice["data"][0]["attributes"]["id"]).to eq(invoice_1.id)
     end
+    it "can find all matches based on merchant_id" do
+      merchant_1 = create(:merchant)
+      customer_1 = create(:customer)
+      item_1 = create(:item, merchant: merchant_1)
+      invoice_1 = create(:invoice, merchant: merchant_1, customer: customer_1)
+
+
+      get "/api/v1/invoices/find_all?merchant_id=#{invoice_1.merchant_id}"
+
+      invoice = JSON.parse(response.body)
+      expect(response).to be_successful
+
+      expect(invoice["data"][0]["attributes"]["id"]).to eq(invoice_1.id)
+    end
   end
 end
